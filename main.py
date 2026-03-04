7fd051044a0e2c3f4f314acd2cf92d3ca2805dca
F-K-C <fakocarvalho@gmail.com>
2026-03-04 17:46:09 -0300

    Implementando o deslocamento

---
+++ main.py
@@ -0,0 +1,9 @@
+
+mensagem = input("Digite a mensagem! ")
+chave = int(input("Digite a chave de deslocamento: "))
+
+print("Mensagem: ", mensagem)
+print("Chave: ", chave)
+
+for cadaLetra in mensagem:
+    print(cadaLetra)
